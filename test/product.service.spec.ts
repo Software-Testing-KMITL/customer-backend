1c169eb37fea61bcafa95818718a06c79a245bb3
Chawapon <chawapon2001@gmail.com>
2023-02-18 13:00:41 +0700

    Update product.service.spec.ts

--- test/product.service.spec.ts
+++ test/product.service.spec.ts
@@ -1,17 +1,21 @@
-import { Product } from './../src/product/product.schema';
+import { Test,TestingModule } from '@nestjs/testing';
 import { ProductDocument } from 'src/product/product.schema';
 import { ProductService } from './../src/product/product.service';
 import { Model } from 'mongoose';
 
 describe('ProductService', () => {
-    let productService: ProductService;
-    let productModel: Model<ProductDocument>,
+  let productService: ProductService;
+  let productModel: Model<ProductDocument>,
+  
+    beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ProductService],
+    }).compile();
 
-        beforeEach(() => {
-        
-    productModel = new Model<ProductDocument>,
-    productService = new ProductService(productModel),
+    service = module.get<ProductService>(ProductService);
   });
+    //   productService = new ProductService(productModel);
+// productModel = new Model<ProductDocument>();
 
   describe('findAll', () => {
     it('should return an array of cats', async () => {
