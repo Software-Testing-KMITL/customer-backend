4071401ed1b30c94a3278dbb265cebfb98e11455
gandastik <gandastik@gmail.com>
2023-02-18 14:03:17 +0700

    ✨ feat: add response api

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -7,15 +7,26 @@ export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Get()
-  async getAllProducts(): Promise<Product[]> {
-    return await this.productService.findAll();
+  async getAllProducts() {
+    const products = await this.productService.findAll();
+    return {
+      status: {
+        code: 200,
+        message: 'get products successfully',
+      },
+      products: [products],
+    };
   }
 
   @Get(':productId')
-  async getProductById(
-    @Param('productId') productId: string,
-  ): Promise<Product> {
-    console.log(productId);
-    return await this.productService.findById(productId);
+  async getProductById(@Param('productId') productId: string) {
+    const product = await this.productService.findById(productId);
+    return {
+      status: {
+        code: 200,
+        message: 'get specific product successfully',
+      },
+      product,
+    };
   }
 }
