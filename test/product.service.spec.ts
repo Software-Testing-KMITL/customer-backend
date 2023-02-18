1c342fac7fc9f06f2c3cba65532d7fcffa5db4a0
Chawapon <chawapon2001@gmail.com>
2023-02-18 12:07:01 +0700

    Update product.service.spec.ts

--- test/product.service.spec.ts
+++ test/product.service.spec.ts
@@ -1,18 +1,26 @@
+import { Product } from './../src/product/product.schema';
+import { ProductDocument } from 'src/product/product.schema';
 import { ProductService } from './../src/product/product.service';
+import { Model } from 'mongoose';
 
 describe('ProductService', () => {
-  let productService: ProductService;
+    let productService: ProductService;
+    let productModel: Model<ProductDocument>,
 
-  beforeEach(() => {
-    productService = new ProductService();
+        beforeEach(() => {
+        
+    productModel = new Model<ProductDocument>,
+    productService = new ProductService(productModel),
   });
 
   describe('findAll', () => {
     it('should return an array of cats', async () => {
       const result = ['test'];
-      jest.spyOn(productService, 'findAll').mockImplementation(() => result);
+      jest
+        .spyOn(productService, 'getAllProducts')
+        .mockImplementation(() => result);
 
-      expect(await productService.findAll()).toBe(result);
+      expect(await productService.getAllProducts()).toBe(result);
     });
   });
 });
