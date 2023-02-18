11e6dda26f4b4c86f17aa63a4824c2ab13cc4a2f
Witchayut Gerdchai <gm@Witchayut-s-MacBook-Pro.local>
2023-02-18 14:01:25 +0700

    ➕ add api doc

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { Product } from './product.schema';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  @ApiOkResponse({
+    description: 'Response all products',
+    type: Product,
+    isArray: true
+  })
   @Get()
   async getAllProducts(): Promise<Product[]> {
     return await this.productService.findAll();
