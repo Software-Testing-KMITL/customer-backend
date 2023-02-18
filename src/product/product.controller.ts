b36c47848e312a3fac02c1e140f9b493746cd00b
gandastik <gandastik@gmail.com>
2023-02-18 11:58:01 +0700

    ✨ feat: add product-schema + get all products

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { Product } from './product.schema';
 
 @Controller('products')
 export class ProductController {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor() {}
+  constructor(private readonly productService: ProductService) {}
 
   @Get()
-  async getAllProducts(): Promise<string> {
-    return 'all products';
+  async getAllProducts(): Promise<Product[]> {
+    return await this.productService.getAllProducts();
   }
 }
