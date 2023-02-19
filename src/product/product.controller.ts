c92c6b85dcd60b6ffecac51079781db8259b7f23
gandastik <gandastik@gmail.com>
2023-02-19 11:34:17 +0700

    ✨ feat: pagination for getAllProducts

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ApiOkResponse } from '@nestjs/swagger';
 import { ResponseProductsProfileDto, ResponseProductProfileDto } from './dtos/product-response.dto';
+import JwtAccessGuard from 'src/shared/guards/jwt-access.guard';
 
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  //TODO: add api docs for query params
   @ApiOkResponse({
     description: 'Response all products',
     type: ResponseProductsProfileDto,
@@ -16,11 +18,22 @@ export class ProductController {
     const products = await this.productService.findAll();
     const { page, perPage } = query;
 
-    // check if there are enough products to display
-    if (page * perPage > products.length) {
-      return { status: { code: 404, message: 'no product to display' } };
+    // check if page and perPage are provided
+    if (page && perPage) {
+      // check if there are enough products to display
+      if ((page - 1) * perPage > products.length) {
+        return { status: { code: 404, message: 'no product to display' } };
+      } else {
+        // return products based on page and perPage
+        return {
+          status: {
+            code: 200,
+            message: 'get products successfully',
+          },
+          products: products.slice((page - 1) * perPage, page * perPage),
+        };
+      }
     }
-
     return {
       status: {
         code: 200,
@@ -34,6 +47,7 @@ export class ProductController {
     description: 'Response all products',
     type: ResponseProductProfileDto,
   })
+  @UseGuards(JwtAccessGuard)
   @Get(':productId')
   async getProductById(@Param('productId') productId: string) {
     const product = await this.productService.findById(productId);
