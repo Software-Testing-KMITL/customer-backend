9b6f4075b12434f0947708475d9d909f3d6a65d5
gandastik <gandastik@gmail.com>
2023-02-19 14:10:07 +0700

    🐛 bug: fixed product response bug

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -3,7 +3,7 @@ import { Product, ProductDocument } from './schemas/product.schema';
 import { Model } from 'mongoose';
 import { CreateProductDto } from './dtos/create-product.dto';
 import { Injectable } from '@nestjs/common';
-import { Category, CategoryDocument } from 'src/category/schemas/category.schema';
+import { Category, CategoryDocument } from '../category/schemas/category.schema';
 
 @Injectable()
 export class ProductService {
@@ -14,17 +14,39 @@ export class ProductService {
 
   async findAll() {
     // populate category field with name and exclude _id
-    const products: Product[] = await this.productModel.find().populate('category', 'name -_id');
+    const products: (Product & { _id: string })[] = await this.productModel.find().populate('category', 'name -_id');
     return products.map((product) => {
       return {
-        ...product,
+        _id: product._id,
+        name: product.name,
+        price: product.price,
+        amount: product.amount,
+        description: product.description,
         category: product.category.map((category) => category.name),
+        picture: product.picture,
       };
     });
   }
 
-  async findById(id: string): Promise<Product> {
-    return await this.productModel.findById(id);
+  async findById(id: string) {
+    const product: Product & { _id: string } = await this.productModel.findById(id).populate('category', 'name -_id');
+    return {
+      _id: product._id,
+      name: product.name,
+      price: product.price,
+      amount: product.amount,
+      description: product.description,
+      category: product.category.map((category) => category.name),
+      picture: product.picture,
+    };
+  }
+
+  async findByCategory(category: string[]) {
+    const categories = category.map(async (item) => {
+      return await this.categoryModel.find({ name: item });
+    });
+
+    return await this.productModel.find({ category: categories });
   }
 
   async create(product: CreateProductDto) {
